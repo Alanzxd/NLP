fdd098447ca8415dfc928ba5c5ef55b4ccacbb88
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2023-11-11 09:29:53 +0800

    Add files via upload

--- prepare-retreival-data-qa.py
+++ prepare-retreival-data-qa.py
@@ -5,89 +5,58 @@
 --dataset_path ./popQA.tsv \
 --num_docs 16
 '''
-print('test')
-import json
+
 import csv
-import sys
+import json
 import argparse
 from tqdm import tqdm
 from transformers import AutoTokenizer
-from ralm.file_utils import print_args
 from ralm.retrievers.retrieval_factory import add_retriever_args, get_retriever
 
-def check_answer_in_context(context, answers):
-    # Placeholder for actual answer checking logic
-    # You'll need to implement this based on your specific needs
-    return any(answer.lower() in context.lower() for answer in answers)
-
 def main(args):
-    # Dump args
-    print_args(args, output_file=args.output_file.replace(".json", ".args.txt"))
-
     print("Loading tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
 
-    print("Loading questions from CSV...")
+    print("Loading dataset...")
     questions_data = []
     with open(args.dataset_path, 'r', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
+        reader = csv.DictReader(csvfile, delimiter='\t')
         for row in reader:
             questions_data.append(row)
 
     print(f"Creating retriever of type {args.retrieval_type}...")
     retriever = get_retriever(args.retrieval_type, args, tokenizer)
 
-    output_data = []
+    retrieval_data = []
     for item in tqdm(questions_data):
-        question = item['question']
         question_id = item['id']
-        possible_answers = json.loads(item['possible_answers'].replace('""', '"'))
+        question_text = item['question']
 
-        # Tokenize the question for retrieval
-        encoded_question = tokenizer(question, return_tensors='pt')
+        # Placeholder for the retrieval logic
+        # Implement your logic to retrieve ctxs and determine hasanswer
+        ctxs = [{"id": "123456", "text": "Dummy context.", "hasanswer": False} for _ in range(5)]
 
-        # Retrieve contexts using the retriever
-        # This part is a placeholder and needs to be implemented based on your retrieval system
-        retrieved_contexts = retriever.retrieve(encoded_question.input_ids, args.num_docs)
-
-        # Check if contexts contain the answer
-        ctxs = []
-        for ctx_id, context in enumerate(retrieved_contexts):
-            has_answer = check_answer_in_context(context, possible_answers)
-            ctxs.append({
-                "id": ctx_id,  # This would actually be a unique identifier for the context
-                "text": context,
-                "hasanswer": has_answer
-            })
-
-        # Construct the output data structure
-        output_data.append({
+        retrieval_data.append({
             "id": question_id,
-            "question": question,
+            "question": question_text,
             "ctxs": ctxs
         })
 
     print(f"Writing to {args.output_file}")
-    with open(args.output_file, "w") as f:
-        json.dump(output_data, f, indent=4)
+    with open(args.output_file, "w", encoding='utf-8') as f:
+        json.dump(retrieval_data, f, indent=4)
 
     print("Done!")
 
 if __name__ == '__main__':
-    assert len(sys.argv) > 1 and sys.argv[1] == "--retrieval_type"
-    retrieval_type = sys.argv[2]
-    assert retrieval_type in ["dense", "sparse"]
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--output_file", required=True, type=str)
     parser.add_argument("--dataset_path", type=str, required=True)
     parser.add_argument("--tokenizer_name", type=str, default="bert-base-uncased")
-
-    # Retrieval params
-    parser.add_argument("--retrieval_type", required=True)
-    parser.add_argument("--num_docs", type=int, default=5)  # Number of contexts to retrieve
-    add_retriever_args(parser, retrieval_type)
+    parser.add_argument("--retrieval_type", required=True, choices=["dense", "sparse"])
+    
+    add_retriever_args(parser, retrieval_type="dense")  # assuming dense is the default
 
     args = parser.parse_args()
     main(args)
