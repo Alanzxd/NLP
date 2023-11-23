acef73bb64c7aa8829a24672e243038b440678b4
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2023-11-24 07:22:10 +0800

    Add files via upload

--- unrar.py
+++ unrar.py
@@ -1,10 +1,10 @@
 import rarfile
 
-# RAR文件的名称
+
 rar_name = 'merged_output.rar'
 
-# 使用rarfile解压
+
 with rarfile.RarFile(rar_name) as opened_rar:
     opened_rar.extractall()
 
-print("解压完成")
+print("ok")
